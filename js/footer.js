5e947d8d97536d90d9cef942ec6c51f508f7651b
Phu Tuan Autonomous <autonomous@Autonomouss-MacBook-Pro.local>
2023-05-23 13:02:55 +0700

    update

---
+++ js/footer.js
@@ -0,0 +1,36 @@
+const footer = document.querySelector('.main-footer')
+
+footer.innerHTML += `
+<div class="container">
+<div class="footer row">
+  <div class="item-footer col-6 col-md-4">
+    <div class="logo-footer">
+      <img src="./images/lgo-1.png (1).webp" alt="" width="100px">
+    </div>
+    <div class="list-footer">
+      <ul>
+        <li><i class="fa fa-building" aria-hidden="true"></i>CÔNG TY TNHH SX VÀ TM NASEA</li>
+        <li><i class="fa fa-map-marker" aria-hidden="true"></i>Địa chỉ : 296C Huỳnh Tấn Phát, Phường Vĩnh Hiệp, Thành phố Rạch Giá</li>
+        <li><i class="fa fa-phone" aria-hidden="true"></i>Điện thoại: 0829595902</li>
+        <li><i class="fa fa-edge" aria-hidden="true"></i>Website: www.nasea.vn</li>
+      </ul>
+    </div>
+  </div>
+  <div class="item-footer col-6 col-md-4">
+    <h3>CHÍNH SÁCH</h3>
+    <ul>
+      <li>Chính sách bảo mật</li>
+      <li>Chính sách thanh toán</li>
+      <li>Chính sách giao hàng</li>
+      <li>Chính sách cam kết</li>
+    </ul>
+  </div>
+  <div class="item-footer col-12 col-md-4">
+    <h3 class='mobile-phuongthuc'>PHƯƠNG THỨC THANH TOÁN</h3>
+    <div class="img-phuongthuc">
+      <img src="./images/bank-payment.webp" alt="" width="50%">
+    </div>
+  </div>
+</div>
+</div>
+`
